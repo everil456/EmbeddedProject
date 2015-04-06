004426179aab46f7e795ada9d4c6c1b3b47b4cce
mstacey359 <markstacey359@gmail.com>
2015-04-06 08:40:38 -0600

    I added in a python spectrograph that i found online

--- matplotlib/spectrograph.py
+++ matplotlib/spectrograph.py
@@ -1,55 +1,116 @@
-"""PyAudio example: Record a few seconds of audio and save to a WAVE file."""
+#!/usr/bin/env python
+# Written by Yu-Jie Lin
+# Public Domain
+#
+# Deps: PyAudio, NumPy, and Matplotlib
+# Blog: http://blog.yjl.im/2012/11/frequency-spectrum-of-sound-using.html
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import pyaudio
+import struct
 import wave
 
-CHUNK = 1024
+
+SAVE = 0.0
+TITLE = ''
+FPS = 25.0
+
+nFFT = 512
+BUF_SIZE = 4 * nFFT
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
-
-p = pyaudio.PyAudio()
-
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
-
-print("* recording")
-
-frames = []
-
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-
-print("* done recording")
-
-stream.stop_stream()
-stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
 
 
-wr = wave.open(WAVE_OUTPUT_FILENAME, 'rb')
+def animate(i, line, stream, wf, MAX_y):
 
-fs = wf.getframerate()
-bytes_per_sample = wf.getsampwidth()
-bits_per_sample  = bytes_per_sample * 8
-dtype = 'int{0}'.format(bits_per_sample)
-channels = wf.getnchannels()
-wr.writeframes(b''.join(frames))
-wr.close()
+  # Read n*nFFT frames from stream, n > 0
+  N = max(stream.get_read_available() / nFFT, 1) * nFFT
+  data = stream.read(N)
+  if SAVE:
+    wf.writeframes(data)
+
+  # Unpack data, LRLRLR...
+  y = np.array(struct.unpack("%dh" % (N * CHANNELS), data)) / MAX_y
+  y_L = y[::2]
+  y_R = y[1::2]
+
+  Y_L = np.fft.fft(y_L, nFFT)
+  Y_R = np.fft.fft(y_R, nFFT)
+
+  # Sewing FFT of two channels together, DC part uses right channel's
+  Y = abs(np.hstack((Y_L[-nFFT/2:-1], Y_R[:nFFT/2])))
+
+  line.set_ydata(Y)
+  return line,
 
 
-audio = np.fromstring(wr.readframes(int(duration*fs*bytes_per_sample/channels)), dtype=dtype)
-audio.shape = (audio.shape[0]/channels, channels)
+def init(line):
 
-print audio
+  # This data is a clear frame for animation
+  line.set_ydata(np.zeros(nFFT - 1))
+  return line,
+
+
+def main():
+  
+  fig = plt.figure()
+
+  # Frequency range
+  x_f = 1.0 * np.arange(-nFFT / 2 + 1, nFFT / 2) / nFFT * RATE
+  ax = fig.add_subplot(111, title=TITLE, xlim=(x_f[0], x_f[-1]),
+                       ylim=(0, 2 * np.pi * nFFT**2 / RATE))
+  ax.set_yscale('symlog', linthreshy=nFFT**0.5)
+
+  line, = ax.plot(x_f, np.zeros(nFFT - 1))
+
+  # Change x tick labels for left channel
+  def change_xlabel(evt):
+    labels = [label.get_text().replace(u'\u2212', '')
+              for label in ax.get_xticklabels()]
+    ax.set_xticklabels(labels)
+    fig.canvas.mpl_disconnect(drawid)
+  drawid = fig.canvas.mpl_connect('draw_event', change_xlabel)
+
+  p = pyaudio.PyAudio()
+  # Used for normalizing signal. If use paFloat32, then it's already -1..1.
+  # Because of saving wave, paInt16 will be easier.
+  MAX_y = 2.0**(p.get_sample_size(FORMAT) * 8 - 1)
+
+  frames = None
+  wf = None
+  if SAVE:
+    frames = int(FPS * SAVE)
+    wf = wave.open('temp.wav', 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+
+  stream = p.open(format=FORMAT,
+                  channels=CHANNELS,
+                  rate=RATE,
+                  input=True,
+                  frames_per_buffer=BUF_SIZE)
+
+  ani = animation.FuncAnimation(fig, animate, frames,
+      init_func=lambda: init(line), fargs=(line, stream, wf, MAX_y),
+      interval=1000.0/FPS, blit=True)
+
+  if SAVE:
+    ani.save('temp.mp4', fps=FPS)
+  else:
+    plt.show()
+
+  stream.stop_stream()
+  stream.close()
+  p.terminate()
+
+  if SAVE:
+    wf.close()
+
+
+if __name__ == '__main__':
+  main()
+
